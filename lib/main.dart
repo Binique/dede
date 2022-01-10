a607b48918d701b90c8c98e7cf0b616c4df12d1b
Leonel Binique <leonel.binique@gmail.com>
2022-01-10 10:33:16 +0100

    Random + Stateless/Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -15,47 +15,50 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+                },
               child:
-              Padding(
-                padding:
-                const EdgeInsets.all(16.0),
-                child:
-                FlatButton(
-                  onPressed:() {
-                    print('bonjour');
-                  },
-                child:
-                Image.asset('images/dice$leftDiceNumber.png'),
-              ),
-          ),
+              Image.asset('images/dice$leftDiceNumber.png'),
+            ),
           ),
           Expanded(
-              child:
-              Padding(
-                padding:
-                const EdgeInsets.all(16.0),
-                child:
-                FlatButton(
-                onPressed:() {
-                print('bonjour');
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
                 },
-                child:
-                Image.asset('images/dice2.png'),
-              ),
-          ),
+              child:
+              Image.asset('images/dice$rightDiceNumber.png'),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
+
+
+
 
