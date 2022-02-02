74eed494f62d88ef057dec646230cfd0d885da05
Leonel Binique <leonel.binique@gmail.com>
2022-02-02 10:34:16 +0100

    Interaction base de données

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:http/http.dart' as http;
+
 void main() {
   return runApp(
     MaterialApp(
@@ -10,24 +12,78 @@ void main() {
           title: Text('Dédé'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePage2(),
       ),
     ),
   );
 }
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-class _DicePageState extends State<DicePage> {
+
+class DicePage extends StatelessWidget {
   int leftDiceNumber = Random().nextInt(6) + 1;
   int rightDiceNumber = Random().nextInt(6) + 1;
 
-  void changeDiceFace() {
-    setState(() {
-      rightDiceNumber = Random().nextInt(6) + 1;
-      leftDiceNumber = Random().nextInt(6) + 1;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                    onPressed: () {
+                      leftDiceNumber = 6;
+                      print("Vous avez appuyé sur l'image de gauche");
+                    },
+                    child: Image.asset("images/dice$leftDiceNumber.png")),
+              )),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                    onPressed: () {
+                      print("Vous avez appuyé sur l'image de droite");
+                    },
+                    child: Image.asset("images/dice$rightDiceNumber.png")),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class DicePage2 extends StatefulWidget {
+  const DicePage2({Key? key}) : super(key: key);
+
+  @override
+  _DicePage2State createState() => _DicePage2State();
+}
+
+class _DicePage2State extends State<DicePage2> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+
+  diceChanged() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
+  sendScore() async {
+    var headers = {
+      'Content-Type': 'application/json',
+    };
+    var request = http.Request('POST', Uri.parse('http://localhost/apitest/public/api/dedes'));
+    request.body = json.encode({"pseudo": "test", "score": leftDiceNumber + rightDiceNumber });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
   }
 
   @override
@@ -36,31 +92,32 @@ class _DicePageState extends State<DicePage> {
       child: Row(
         children: [
           Expanded(
-            child: TextButton(
-              onPressed: () {
-                changeDiceFace();
-                },
-              child:
-              Image.asset('images/dice$leftDiceNumber.png'),
-            ),
-          ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        diceChanged();
+                      });
+                      sendScore();
+                      print("Vous avez appuyé sur l'image de gauche");
+                    },
+                    child: Image.asset("images/dice$leftDiceNumber.png")),
+              )),
           Expanded(
-            child: TextButton(
-              onPressed: () {
-                changeDiceFace();
-                },
-              child:
-              Image.asset('images/dice$rightDiceNumber.png'),
-            ),
-          ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        diceChanged();
+                      });
+                      print("Vous avez appuyé sur l'image de droite");
+                    },
+                    child: Image.asset("images/dice$rightDiceNumber.png")),
+              )),
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
